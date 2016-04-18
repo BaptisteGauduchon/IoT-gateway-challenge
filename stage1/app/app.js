4b597cc2a8850df403a7585526fa78483b44c197
Baptiste <baptiste.g44@gmail.com>
2016-04-18 14:39:38 +0200

    resolved issue wherre header was modified after request was sent

--- stage1/app/app.js
+++ stage1/app/app.js
@@ -21,6 +21,9 @@ app.use(function(err, req, res, next) {
 
 // catch-all error handler
 app.use(function(err, req, res, next) {
+	if (res.headersSent) {
+		return next(err);
+	}
 	res.status(err.status || 500).json({error : err});
 });
 
