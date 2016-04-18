fe3943f6bde1af823de6ff105a2171c653863fd0
Baptiste <baptiste.g44@gmail.com>
2016-04-18 14:40:44 +0200

    code correction
    message was created even if validation failed

--- stage1/app/routes/message.js
+++ stage1/app/routes/message.js
@@ -12,26 +12,25 @@ router.post('/messages', function(req, res, next) {
 	msgValidator.validate(req.body, function(err) {
 		if(err) {
 			next({status : 400, message : err});
+		} else {
+			// create the message document from request & convert date to millisecond for storage
+			var sensorMessage = {
+				id : req.body.id,
+				timestamp: Date.parse(req.body.timestamp),
+				sensorType: req.body.sensorType,
+				value: req.body.value
+			};
+
+			// store message
+			storage.addMessage(sensorMessage, function(err,message) {
+				if (err) {
+					// return error (duplicated message id for instance)
+					next({status : 403, message : err});
+				} else {
+					res.end();
+				}
+			});
 		}
-
-		// create the message document from request & convert date to millisecond for storage
-		var sensorMessage = {
-			id : req.body.id,
-			// TODO : homegenize date format with storage for date comparison
-			timestamp: Date.parse(req.body.timestamp),
-			sensorType: req.body.sensorType,
-			value: req.body.value
-		};
-
-		// store message
-		storage.addMessage(sensorMessage, function(err,message) {
-			if (err) {
-				// return error (duplicated message id for instance)
-				next({status : 403, message : err});
-			} else {
-				res.end();
-			}
-		});
 	});
 });
 
